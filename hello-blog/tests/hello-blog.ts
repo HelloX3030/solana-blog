4c183fd8f0545a45b6eed43539dbc2be25f89f49
HelloX3030 <120511705+HelloX3030@users.noreply.github.com>
2025-08-28 09:14:41 +0200

    added testing

--- hello-blog/tests/hello-blog.ts
+++ hello-blog/tests/hello-blog.ts
@@ -1,16 +1,69 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { HelloBlog } from "../target/types/hello_blog";
+import { CommitLog } from "../target/types/commit_log";
+import { assert } from "chai";
 
-describe("hello-blog", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("commit-log", () => {
+  // Use local cluster
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.helloBlog as Program<HelloBlog>;
+  const program = anchor.workspace.CommitLog as Program<CommitLog>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creates a commit entry!", async () => {
+    const entryKeypair = anchor.web3.Keypair.generate();
+
+    // Create the commit entry
+    await program.methods
+      .createEntry("First commit", "Hello blockchain!")
+      .accounts({
+        entry: entryKeypair.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([entryKeypair])
+      .rpc();
+
+    // Fetch and assert
+    const account = await program.account.commitEntry.fetch(entryKeypair.publicKey);
+    assert.equal(account.title, "First commit");
+    assert.equal(account.description, "Hello blockchain!");
+  });
+
+  it("Updates a commit entry!", async () => {
+    const entryKeypair = anchor.web3.Keypair.generate();
+
+    // First, create the entry
+    await program.methods
+      .createEntry("Default Title", "Default Description")
+      .accounts({
+        entry: entryKeypair.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([entryKeypair])
+      .rpc();
+
+    // Update the title
+    await program.methods
+      .updateTitle("New Title")
+      .accounts({
+        entry: entryKeypair.publicKey,
+      }).rpc();
+    {
+		const updatedAccount = await program.account.commitEntry.fetch(entryKeypair.publicKey);
+		assert.equal(updatedAccount.title, "New Title");
+	}
+
+	// Update the Description
+	await program.methods
+	  .updateDescription("New Description")
+	  .accounts({
+		entry: entryKeypair.publicKey,
+	  }).rpc();
+	{
+		const updatedAccount = await program.account.commitEntry.fetch(entryKeypair.publicKey);
+		assert.equal(updatedAccount.description, "New Description");
+	}
   });
 });
